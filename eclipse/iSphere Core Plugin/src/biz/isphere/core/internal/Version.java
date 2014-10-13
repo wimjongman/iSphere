d3d56b8a30049edc66ee198aa867c36f02a73c19
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2014-10-13 17:50:58 +0000

    Improved class "Version" to support beta version numbers.

--- eclipse/iSphere Core Plugin/src/biz/isphere/core/internal/Version.java
+++ eclipse/iSphere Core Plugin/src/biz/isphere/core/internal/Version.java
@@ -11,11 +11,17 @@
 
 package biz.isphere.core.internal;
 
+import biz.isphere.base.internal.StringHelper;
+
 public class Version implements Comparable<Version> {
 
     private String version;
+    private boolean isChecked;
 
     public final String get() {
+        if (!isChecked) {
+            this.version = checkAndFixParts(this.version);
+        }
         return this.version;
     }
 
@@ -24,23 +30,85 @@ public class Version implements Comparable<Version> {
     }
 
     public Version(String version) {
-        if (version == null) throw new IllegalArgumentException("Version can not be null");
-        if (!version.matches("[0-9]+(\\.[0-9]+)*")) throw new IllegalArgumentException("Invalid version format");
+        if (version == null) {
+            throw new IllegalArgumentException("Version can not be null");
+        }
+        if (!version.matches("[0-9]+(\\.[0-9]+)*" + "(\\.b[0-9]+)?")) {
+            throw new IllegalArgumentException("Invalid version format");
+        }
         this.version = version;
+        this.isChecked = false;
+    }
+
+    private String checkAndFixParts(String version) {
+        String[] parts = splitt(version);
+        StringBuilder fixed = new StringBuilder();
+        for (String part : parts) {
+            if (fixed.length() > 0) {
+                fixed.append(".");
+            }
+            fixed.append(removeLeadingZeros(part));
+        }
+        return fixed.toString();
+    }
+
+    private String removeLeadingZeros(String part) {
+        if (part.startsWith("b")) {
+            return "b" + removeLeadingZeros(part.substring(1));
+        }
+        String fixed = part.replaceAll("^0+", "");
+        if (StringHelper.isNullOrEmpty(fixed)) {
+            return "0";
+        }
+        return fixed;
     }
 
     public int compareTo(Version that) {
         if (that == null) return 1;
-        String[] thisParts = this.get().split("\\.");
-        String[] thatParts = that.get().split("\\.");
+        String[] thisParts = splitt(this.get());
+        String[] thatParts = splitt(that.get());
         int length = Math.max(thisParts.length, thatParts.length);
         for (int i = 0; i < length; i++) {
-            int thisPart = i < thisParts.length ? Integer.parseInt(thisParts[i]) : 0;
-            int thatPart = i < thatParts.length ? Integer.parseInt(thatParts[i]) : 0;
+            int thisPart = getPart(thisParts, thatParts, i);
+            int thatPart = getPart(thatParts, thisParts, i);
+            
             if (thisPart < thatPart) return -1;
             if (thisPart > thatPart) return 1;
         }
         return 0;
+    }
+
+    private String[] splitt(String version) {
+        return version.split("\\.");
+    }
+    
+    private int getPart(String[] thisParts, String[] thatParts, int i) {
+        if (thisParts.length < thatParts.length && i >= thisParts.length) {
+            if (isBetaPart(thatParts[i])) {
+                return Integer.MAX_VALUE;
+            } else {
+                return 0;
+            }
+        }
+        int part =  parseInteger(thisParts[i]);
+        if (part == Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("Illegal release number. Release number must not contain: " + Integer.MAX_VALUE);
+        }
+        return part;
+    }
+
+    private int parseInteger(String part) {
+        if (isBetaPart(part)) {
+            return Integer.parseInt(part.substring(1));
+        }
+        return Integer.parseInt(part);
+    }
+
+    private boolean isBetaPart(String part) {
+        if (StringHelper.isNullOrEmpty(part)) {
+            return false;
+        }
+        return part.startsWith("b");
     }
 
     @Override
---
+++ rse2 (RDi)/iSphere JUnit (RDi)/src/biz/isphere/junit/TestVersion.java
@@ -0,0 +1,101 @@
+package biz.isphere.junit;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import biz.isphere.core.internal.Version;
+
+public class TestVersion {
+
+    @Test
+    public void testIllegalVersionNumbers() throws Exception {
+
+        try {
+            Version a = new Version(null);
+            Assert.fail("NULL is not allowed for the constructor.");
+        } catch (IllegalArgumentException e) {
+            // exception see ==> OK
+            Assert.assertEquals("Version can not be null", e.getMessage());
+        }
+
+        try {
+            Version a = new Version("2.1.a");
+            Assert.fail("Version number must not contain characters.");
+        } catch (IllegalArgumentException e) {
+            // exception see ==> OK
+            Assert.assertEquals("Invalid version format", e.getMessage());
+        }
+
+        try {
+            Version a = new Version(".1.1");
+            Assert.fail("Version number must not start with a dot.");
+        } catch (IllegalArgumentException e) {
+            // exception see ==> OK
+            Assert.assertEquals("Invalid version format", e.getMessage());
+        }
+
+        try {
+            Version a = new Version("2.1.1.b");
+            Assert.fail("Beta part must specify the beta number.");
+        } catch (IllegalArgumentException e) {
+            // exception see ==> OK
+            Assert.assertEquals("Invalid version format", e.getMessage());
+        }
+
+        try {
+            Version a = new Version("2.b1.1");
+            Assert.fail("Beta part must be at the end.");
+        } catch (IllegalArgumentException e) {
+            // exception see ==> OK
+            Assert.assertEquals("Invalid version format", e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void testLegalVersionNumbers() throws Exception {
+
+        Assert.assertTrue(new Version("1.1").compareTo(new Version("1.1.1")) < 0);
+        Assert.assertFalse(new Version("1.1").equals(new Version("1.1.1")));
+
+        Assert.assertTrue(new Version("2.0").compareTo(new Version("1.9.9")) > 0);
+        Assert.assertFalse(new Version("2.0").equals(new Version("1.9.9")));
+
+        Assert.assertTrue(new Version("1.0").compareTo(new Version("1")) == 0);
+        Assert.assertTrue(new Version("1.0").equals(new Version("1")));
+
+        Assert.assertTrue(new Version("1.0").compareTo(null) > 0);
+        Assert.assertFalse(new Version("1.0").equals(null));
+
+        List<Version> versions = new ArrayList<Version>();
+        versions.add(new Version("1.00.1")); // max version
+        versions.add(new Version("1.0.5"));
+        versions.add(new Version("1.01.0"));
+        versions.add(new Version("2")); // min version
+        String minVersion = Collections.min(versions).get();
+        String maxVersion = Collections.max(versions).get();
+
+        Assert.assertEquals("1.0.1", minVersion);
+        Assert.assertEquals("2", maxVersion);
+
+        Version a = new Version("2.06");
+        Version b = new Version("2.060");
+        Assert.assertFalse(a.equals(b));
+    }
+
+    @Test
+    public void testBetaVersionNumbers() throws Exception {
+
+        Assert.assertTrue(new Version("2.4.2").compareTo(new Version("2.4.2.b16")) > 0);
+        Assert.assertTrue(new Version("2.4.2.b010").compareTo(new Version("2.4.2.b010")) == 0);
+        Assert.assertTrue(new Version("2.4.2.b5").compareTo(new Version("2.4.2.b6")) < 0);
+        Assert.assertTrue(new Version("2.4.b1").compareTo(new Version("2.4.b2")) < 0);
+
+    }
+
+}
