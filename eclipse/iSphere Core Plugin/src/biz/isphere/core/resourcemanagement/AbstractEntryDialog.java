b06a145fba909f705f3a4a79a88d8210aca619fd
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2014-10-13 11:21:29 +0000

    Changed to inherit from XDialog, in order to remember the screen dimension as well as the last repository filter path.

--- eclipse/iSphere Core Plugin/src/biz/isphere/core/resourcemanagement/AbstractEntryDialog.java
+++ eclipse/iSphere Core Plugin/src/biz/isphere/core/resourcemanagement/AbstractEntryDialog.java
@@ -12,297 +12,324 @@ import java.io.File;
 import java.io.IOException;
 
 import org.eclipse.jface.action.StatusLineManager;
-import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
+import biz.isphere.base.jface.dialogs.XDialog;
 import biz.isphere.core.ISpherePlugin;
 import biz.isphere.core.Messages;
 import biz.isphere.core.internal.Size;
 
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.events.ModifyEvent;
+public abstract class AbstractEntryDialog extends XDialog {
 
-public abstract class AbstractEntryDialog extends Dialog {
+    private static final String REPOSITORY_PATH = "repositoryPath";
+    
+    private Shell shell;
+    private Button buttonBoth;
+    private Button buttonWorkspace;
+    private Button buttonRepository;
+    private Composite compositeWorkspace;
+    private Composite compositeRepository;
+    private Text textRepository;
+    private StatusLineManager statusLineManager;
+    private Button okButton;
 
-	private Shell shell;
-	private Button buttonBoth;
-	private Button buttonWorkspace;
-	private Button buttonRepository;
-	private Composite compositeWorkspace;
-	private Composite compositeRepository;
-	private Text textRepository;
-	private StatusLineManager statusLineManager;
-	private Button okButton;
-	
-	public AbstractEntryDialog(Shell parentShell) {
-		super(parentShell);
-		setShellStyle(getShellStyle() | SWT.RESIZE);
-		this.shell = parentShell;
-	}
-	
-	protected Control createDialogArea(Composite parent) {
-		
-		Composite container = (Composite) super.createDialogArea(parent);
-		container.setLayout(new GridLayout(1, false));
-		
-		final Group groupEditingArea = new Group(container, SWT.NONE);
-		groupEditingArea.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		groupEditingArea.setText(Messages.Editing_area);
-		final GridLayout gridLayoutStatus = new GridLayout();
-		gridLayoutStatus.numColumns = 1;
-		groupEditingArea.setLayout(gridLayoutStatus);
+    public AbstractEntryDialog(Shell parentShell) {
+        super(parentShell);
+        setShellStyle(getShellStyle() | SWT.RESIZE);
+        this.shell = parentShell;
+    }
 
-		buttonBoth = new Button(groupEditingArea, SWT.RADIO);
-		buttonBoth.setText(Messages.Edit_workspace_and_repository + " " + getSubject());
-		buttonBoth.setSelection(true);
-		buttonBoth.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				compositeWorkspace.setVisible(true);
-				compositeRepository.setVisible(true);
-			    check();
-			}
-		});
+    protected Control createDialogArea(Composite parent) {
 
-		buttonWorkspace = new Button(groupEditingArea, SWT.RADIO);
-		buttonWorkspace.setText(Messages.Edit_only_workspace + " " + getSubject());
-		buttonWorkspace.setSelection(false);
-		buttonWorkspace.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				compositeWorkspace.setVisible(true);
-				compositeRepository.setVisible(false);
-			    check();
-			}
-		});
+        Composite container = (Composite)super.createDialogArea(parent);
+        container.setLayout(new GridLayout(1, false));
 
-		buttonRepository = new Button(groupEditingArea, SWT.RADIO);
-		buttonRepository.setText(Messages.Edit_only_repository + " " + getSubject());
-		buttonRepository.setSelection(false);
-		buttonRepository.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				compositeWorkspace.setVisible(false);
-				compositeRepository.setVisible(true);
-			    check();
-			}
-		});
+        final Group groupEditingArea = new Group(container, SWT.NONE);
+        groupEditingArea.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+        groupEditingArea.setText(Messages.Editing_area);
+        final GridLayout gridLayoutStatus = new GridLayout();
+        gridLayoutStatus.numColumns = 1;
+        groupEditingArea.setLayout(gridLayoutStatus);
 
-		if (needWorkspaceArea()) {
-			
-			compositeWorkspace = new Composite(container, SWT.NONE);
-			compositeWorkspace.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-			compositeWorkspace.setVisible(true);
-			
-			configureWorkspaceArea(compositeWorkspace);
-			
-		}
-		
-		compositeRepository = new Composite(container, SWT.NONE);
-		compositeRepository.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		compositeRepository.setLayout(new GridLayout(3, false));
-		compositeRepository.setVisible(true);
-		
-		Label labelRepository = new Label(compositeRepository, SWT.NONE);
-		labelRepository.setText(Messages.Repository + ":");
-		labelRepository.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1));
-		
-		textRepository = new Text(compositeRepository, SWT.BORDER);
-		textRepository.setText("");
-		textRepository.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		textRepository.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent event) {
-			    check();
-			}
-		});
-		
-		Button browseRepository = new Button(compositeRepository, SWT.NONE);
-		browseRepository.setText(Messages.Browse);
-		browseRepository.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
+        buttonBoth = new Button(groupEditingArea, SWT.RADIO);
+        buttonBoth.setText(Messages.Edit_workspace_and_repository + " " + getSubject());
+        buttonBoth.setSelection(true);
+        buttonBoth.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                compositeWorkspace.setVisible(true);
+                compositeRepository.setVisible(true);
+                check();
+            }
+        });
+
+        buttonWorkspace = new Button(groupEditingArea, SWT.RADIO);
+        buttonWorkspace.setText(Messages.Edit_only_workspace + " " + getSubject());
+        buttonWorkspace.setSelection(false);
+        buttonWorkspace.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                compositeWorkspace.setVisible(true);
+                compositeRepository.setVisible(false);
+                check();
+            }
+        });
+
+        buttonRepository = new Button(groupEditingArea, SWT.RADIO);
+        buttonRepository.setText(Messages.Edit_only_repository + " " + getSubject());
+        buttonRepository.setSelection(false);
+        buttonRepository.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                compositeWorkspace.setVisible(false);
+                compositeRepository.setVisible(true);
+                check();
+            }
+        });
+
+        if (needWorkspaceArea()) {
+
+            compositeWorkspace = new Composite(container, SWT.NONE);
+            compositeWorkspace.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+            compositeWorkspace.setVisible(true);
+
+            configureWorkspaceArea(compositeWorkspace);
+
+        }
+
+        compositeRepository = new Composite(container, SWT.NONE);
+        compositeRepository.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+        compositeRepository.setLayout(new GridLayout(3, false));
+        compositeRepository.setVisible(true);
+
+        Label labelRepository = new Label(compositeRepository, SWT.NONE);
+        labelRepository.setText(Messages.Repository + ":");
+        labelRepository.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1));
+
+        textRepository = new Text(compositeRepository, SWT.BORDER);
+        textRepository.setText("");
+        textRepository.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+        textRepository.addModifyListener(new ModifyListener() {
+            public void modifyText(ModifyEvent event) {
+                check();
+            }
+        });
+
+        Button browseRepository = new Button(compositeRepository, SWT.NONE);
+        browseRepository.setText(Messages.Browse);
+        browseRepository.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
                 FileDialog dialog = new FileDialog(shell, SWT.OK);
-                dialog.setFilterNames(new String[] {getFileSubject()});
-                dialog.setFilterExtensions(new String[] { "*." + getFileExtension()});
-                dialog.setFilterPath("C:\\");
+                dialog.setFilterNames(new String[] { getFileSubject() });
+                dialog.setFilterExtensions(new String[] { "*." + getFileExtension() });
+                dialog.setFilterPath(loadRepositoryPath());
                 // dialog.setFileName("export.xls");
                 // dialog.setOverwrite(true);
                 String file = dialog.open();
                 if (file != null) {
-                	textRepository.setText(file);
+                    textRepository.setText(file);
+                    storeRepositoryPath(dialog.getFilterPath());
                 }
-			}
-		});
-		
-		statusLineManager = new StatusLineManager(); 
+            }
+        });
+
+        statusLineManager = new StatusLineManager();
         statusLineManager.createControl(container, SWT.NONE);
-        Control statusLine = statusLineManager.getControl();        
-		final GridData gridDataStatusLine = new GridData(SWT.FILL, SWT.CENTER, true, false);
-		statusLine.setLayoutData(gridDataStatusLine);
+        Control statusLine = statusLineManager.getControl();
+        final GridData gridDataStatusLine = new GridData(SWT.FILL, SWT.CENTER, true, false);
+        statusLine.setLayoutData(gridDataStatusLine);
 
-		check();
-		
-		return container;
-		
-	}
+        check();
 
-	private void check() {
+        return container;
 
-		if (needWorkspaceArea() && (buttonBoth.getSelection() || buttonWorkspace.getSelection())) {
-			
-			String error = checkWorkspaceArea();
-			if (error != null) {
-				if (okButton != null) okButton.setEnabled(false);
-			    setErrorMessage(error);
-				return;
-			}
-			
-		}
-		
-		if (buttonBoth.getSelection() || buttonRepository.getSelection()) {
+    }
 
-			String fileName = textRepository.getText().trim();
-			if (fileName.equals("")) {
-				if (okButton != null) okButton.setEnabled(false);
-			    setErrorMessage(Messages.Enter_a_file_name + ".");
-			    textRepository.setFocus();
-				return;
-			}
+    private void check() {
 
-			File repository = new File(fileName);
-		    try {
-		    	repository.getCanonicalPath();
-		    }
-		    catch (IOException e) {
-		    	if (okButton != null) okButton.setEnabled(false);
-			    setErrorMessage(Messages.Invalid_file_name + ".");
-			    textRepository.setFocus();
-				return;
-		    }
+        if (needWorkspaceArea() && (buttonBoth.getSelection() || buttonWorkspace.getSelection())) {
 
-			if (!fileName.endsWith("." + getFileExtension())) {
-				if (okButton != null) okButton.setEnabled(false);
-			    setErrorMessage(Messages.File_name_does_not_end_with + " ." + getFileExtension());
-			    textRepository.setFocus();
-				return;
-			}
+            String error = checkWorkspaceArea();
+            if (error != null) {
+                if (okButton != null) okButton.setEnabled(false);
+                setErrorMessage(error);
+                return;
+            }
 
-		}
-		
-		if (okButton != null) okButton.setEnabled(true);
-	    setErrorMessage(null);
-	    
-	}
+        }
 
-	private void setErrorMessage(String errorMessage) {
-		if (errorMessage != null) {
-			statusLineManager.setErrorMessage(ISpherePlugin.getDefault().getImageRegistry().get(ISpherePlugin.IMAGE_ERROR), errorMessage);
-		}
-		else {
-			statusLineManager.setErrorMessage(null, null);
-		}
-	}
+        if (buttonBoth.getSelection() || buttonRepository.getSelection()) {
 
-	protected void okPressed() {
+            String fileName = textRepository.getText().trim();
+            if (fileName.equals("")) {
+                if (okButton != null) okButton.setEnabled(false);
+                setErrorMessage(Messages.Enter_a_file_name + ".");
+                textRepository.setFocus();
+                return;
+            }
 
-		boolean run = true;
+            File repository = new File(fileName);
+            try {
+                repository.getCanonicalPath();
+            } catch (IOException e) {
+                if (okButton != null) okButton.setEnabled(false);
+                setErrorMessage(Messages.Invalid_file_name + ".");
+                textRepository.setFocus();
+                return;
+            }
 
-		if (isEditBoth() || isEditRepository()) {
-			
-			String repository = getRepository();
-			
-			File file = new File(repository);
-			if (!file.exists()) {
+            if (!fileName.endsWith("." + getFileExtension())) {
+                if (okButton != null) okButton.setEnabled(false);
+                setErrorMessage(Messages.File_name_does_not_end_with + " ." + getFileExtension());
+                textRepository.setFocus();
+                return;
+            }
 
-				MessageBox messageBox = new MessageBox(getShell(), SWT.ICON_QUESTION | SWT.YES | SWT.NO);
-				messageBox.setMessage(Messages.Create_the_repository + "?");
-				messageBox.setText(Messages.Repository_does_not_exist);
-				int response = messageBox.open();
+        }
 
-				if (response == SWT.YES) {
-					if (!createEmptyRepository(file)) {
-						run = false;
-					}
-				}
-				else {
-					run = false;
-				}
+        if (okButton != null) okButton.setEnabled(true);
+        setErrorMessage(null);
 
-			}
-			
-		}
-		
-		if (run) {
-			run();
-		}
-		
-		super.okPressed();
-		
-	}
+    }
 
-	protected void createButtonsForButtonBar(Composite parent) {
-		okButton = createButton(parent, IDialogConstants.OK_ID, Messages.OK, false);
-		okButton.setEnabled(false);
-		createButton(parent, IDialogConstants.CANCEL_ID, Messages.Cancel, false);
-	}
+    private void setErrorMessage(String errorMessage) {
+        if (errorMessage != null) {
+            statusLineManager.setErrorMessage(ISpherePlugin.getDefault().getImageRegistry().get(ISpherePlugin.IMAGE_ERROR), errorMessage);
+        } else {
+            statusLineManager.setErrorMessage(null, null);
+        }
+    }
 
-	protected void configureShell(Shell newShell) {
-		super.configureShell(newShell);
-		newShell.setText(getTitle());
-	}
-	
-	protected Point getInitialSize() {
-		return getShell().computeSize(Size.getSize(500), SWT.DEFAULT, true);
-	}
+    protected void okPressed() {
 
-	public String getRepository() {
-		return textRepository.getText();
-	}
+        boolean run = true;
 
-	public boolean isEditBoth() {
-		return buttonBoth.getSelection();
-	}
+        if (isEditBoth() || isEditRepository()) {
 
-	public boolean isEditWorkspace() {
-		return buttonWorkspace.getSelection();
-	}
+            String repository = getRepository();
 
-	public boolean isEditRepository() {
-		return buttonRepository.getSelection();
-	}
-	
-	protected boolean needWorkspaceArea() {
-		return false;
-	}
+            File file = new File(repository);
+            if (!file.exists()) {
 
-	protected void configureWorkspaceArea(Composite composite) {}
+                MessageBox messageBox = new MessageBox(getShell(), SWT.ICON_QUESTION | SWT.YES | SWT.NO);
+                messageBox.setMessage(Messages.Create_the_repository + "?");
+                messageBox.setText(Messages.Repository_does_not_exist);
+                int response = messageBox.open();
 
-	protected String checkWorkspaceArea() {
-		return null;
-	};
-	
-	protected abstract String getTitle();
+                if (response == SWT.YES) {
+                    if (!createEmptyRepository(file)) {
+                        run = false;
+                    }
+                } else {
+                    run = false;
+                }
 
-	protected abstract String getFileSubject();
+            }
 
-	protected abstract String getSubject();
-	
-	protected abstract String getFileExtension();
+        }
 
-	protected abstract void run();
-	
-	protected abstract boolean createEmptyRepository(File repository);
-	
+        if (run) {
+            run();
+        }
+
+        super.okPressed();
+
+    }
+
+    protected void createButtonsForButtonBar(Composite parent) {
+        okButton = createButton(parent, IDialogConstants.OK_ID, Messages.OK, false);
+        okButton.setEnabled(false);
+        createButton(parent, IDialogConstants.CANCEL_ID, Messages.Cancel, false);
+    }
+
+    protected void configureShell(Shell newShell) {
+        super.configureShell(newShell);
+        newShell.setText(getTitle());
+    }
+
+    @Override
+    protected Point getDefaultSize() {
+        return getShell().computeSize(Size.getSize(500), SWT.DEFAULT, true);
+    }
+
+    public String getRepository() {
+        return textRepository.getText();
+    }
+
+    public boolean isEditBoth() {
+        return buttonBoth.getSelection();
+    }
+
+    public boolean isEditWorkspace() {
+        return buttonWorkspace.getSelection();
+    }
+
+    public boolean isEditRepository() {
+        return buttonRepository.getSelection();
+    }
+
+    protected boolean needWorkspaceArea() {
+        return false;
+    }
+
+    protected void configureWorkspaceArea(Composite composite) {
+    }
+
+    protected String checkWorkspaceArea() {
+        return null;
+    };
+
+    protected abstract String getTitle();
+
+    protected abstract String getFileSubject();
+
+    protected abstract String getSubject();
+
+    protected abstract String getFileExtension();
+
+    protected abstract void run();
+
+    protected abstract boolean createEmptyRepository(File repository);
+
+    /**
+     * Restores the screen values of the last search search.
+     */
+    protected String loadRepositoryPath() {
+        
+        return loadValue(REPOSITORY_PATH, "C:\\");
+    }
+
+    /**
+     * Stores the screen values that are preserved for the next search.
+     */
+    protected void storeRepositoryPath(String path) {
+        
+        storeValue(REPOSITORY_PATH, path);
+    }
+
+    /**
+     * Overridden to let {@link XDialog} store the state of this dialog in a
+     * separate section of the dialog settings file.
+     */
+    @Override
+    protected IDialogSettings getDialogBoundsSettings() {
+        return super.getDialogBoundsSettings(ISpherePlugin.getDefault().getDialogSettings());
+    }
+
 }
