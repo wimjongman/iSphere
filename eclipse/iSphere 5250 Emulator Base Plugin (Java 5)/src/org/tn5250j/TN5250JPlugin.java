acec8fc771b71f0f141295f7ee234f549a187b8b
f_hildebrandt <f_hildebrandt@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2015-06-23 10:54:38 +0000

    iSphere 5250 Emulator.

---
+++ eclipse/iSphere 5250 Emulator Base Plugin (Java 5)/src/org/tn5250j/TN5250JPlugin.java
@@ -0,0 +1,45 @@
+// ISPHERE - NEW - START
+
+package org.tn5250j;
+
+import java.util.ArrayList;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+public class TN5250JPlugin extends Plugin {
+
+	public static final String PLUGIN_ID = "org.tn5250j";
+	private static TN5250JPlugin plugin;
+	private static ArrayList<String> tn5250jInstallations = new ArrayList<String>();
+
+	public TN5250JPlugin() {
+		plugin = this;
+	}
+
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		
+		TN5250JPlugin.setTN5250JInstallation("TN5250J - Version 0.6.0.2");
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	public static TN5250JPlugin getDefault() {
+		return plugin;
+	}
+	
+	public static ArrayList getTN5250JInstallations() {
+		return tn5250jInstallations;
+	}
+	
+	public static void setTN5250JInstallation(String installation) {
+		tn5250jInstallations.add(installation);
+	}
+
+}
+
+//ISPHERE - NEW - END
