bf10652977865338197ffe35cc5be0190e3e5b5b
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2015-10-31 17:50:46 +0000

    Added missing copyright header.

--- rse2 (RDi)/iSphere JUnit (RDi)/src/biz/isphere/examples/swt/hexeditor/HexEditDemo.java
+++ rse2 (RDi)/iSphere JUnit (RDi)/src/biz/isphere/examples/swt/hexeditor/HexEditDemo.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2015 iSphere Project Owners
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *******************************************************************************/
+
 package biz.isphere.examples.swt.hexeditor;
 
 import java.nio.ByteBuffer;
@@ -7,7 +15,6 @@ import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
