cc27870aa5f54878d9c88776f1668f3b31dfa5fd
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2015-11-19 15:55:16 +0000

    Added missing copyright headers.

---
+++ rse2 (RDi)/iSphere Compare Filters Compile-Dummy (RDi)/src/org/eclipse/compare/ISphereCompareFilterCompilePlugin.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2015 iSphere Project Owners
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.compare;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class ISphereCompareFilterCompilePlugin implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		ISphereCompareFilterCompilePlugin.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		ISphereCompareFilterCompilePlugin.context = null;
+	}
+
+}
