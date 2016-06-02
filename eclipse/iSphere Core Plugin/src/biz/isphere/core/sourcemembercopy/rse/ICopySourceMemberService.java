3e6caa280d77bfa8dc89dc3e3857d5f28a09cf36
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2016-06-02 06:25:06 +0000

    Removed obsolete interface.

--- eclipse/iSphere Core Plugin/src/biz/isphere/core/sourcemembercopy/rse/ICopySourceMemberService.java
+++
@@ -1,8 +0,0 @@
-package biz.isphere.core.sourcemembercopy.rse;
-
-public interface ICopySourceMemberService {
-
-    public boolean copySourceMember(String fromConnectionName, String fromLibraryName, String fromFileName, String fromMemberName,
-        String toConnectionName, String toLibraryName, String toFileName, String toMemberName);
-
-}
