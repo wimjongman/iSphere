fb7cfdde0bee6dfdd94b48efdb78f5f1589e807e
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2016-08-28 20:12:41 +0000

    Initial import.

---
+++ development/iSphere Development Tools (RDi)/src/biz/isphere/development/objectcontributions/ObjectContributionAction.java
@@ -0,0 +1,35 @@
+package biz.isphere.development.objectcontributions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class ObjectContributionAction implements IObjectActionDelegate {
+
+    @Override
+    public void run(IAction action) {
+        System.out.println("run: " + action.getId());
+    }
+
+    @Override
+    public void selectionChanged(IAction action, ISelection selection) {
+        System.out.println("selectionChanged: " + action.getId());
+        if (selection instanceof StructuredSelection) {
+            StructuredSelection structuredSelection = (StructuredSelection)selection;
+            Object element = structuredSelection.getFirstElement();
+            if (element != null) {
+                System.out.println("  ==> " + element + " (" + element.getClass().getName() + ")");
+            } else {
+                System.out.println("  ==> [null]");
+            }
+        }
+    }
+
+    @Override
+    public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+        System.out.println("setActivePart: " + action.getId());
+    }
+
+}
---
+++ development/iSphere Development Tools (RDi)/src/biz/isphere/development/views/SelectionServiceView.java
@@ -0,0 +1,113 @@
+package biz.isphere.development.views;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IMarkSelection;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.text.TextViewer;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.model.WorkbenchLabelProvider;
+import org.eclipse.ui.part.PageBook;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * This view simply mirrors the current selection in the workbench window. It
+ * works for both, element and text selection.
+ */
+public class SelectionServiceView extends ViewPart {
+
+    private PageBook pagebook;
+    private TableViewer tableviewer;
+    private TextViewer textviewer;
+
+    // the listener we register with the selection service
+    private ISelectionListener listener = new ISelectionListener() {
+        public void selectionChanged(IWorkbenchPart sourcepart, ISelection selection) {
+            // we ignore our own selections
+            if (sourcepart != SelectionServiceView.this) {
+                showSelection(sourcepart, selection);
+            }
+        }
+    };
+
+    /**
+     * Shows the given selection in this view.
+     */
+    public void showSelection(IWorkbenchPart sourcepart, ISelection selection) {
+        setContentDescription(sourcepart.getTitle() + " (" + selection.getClass().getName() + ")");
+        if (selection instanceof IStructuredSelection) {
+            IStructuredSelection ss = (IStructuredSelection)selection;
+            showText(getClassNames(ss.toArray()));
+            // showItems(ss.toArray());
+        } else if (selection instanceof ITextSelection) {
+            ITextSelection ts = (ITextSelection)selection;
+            showText(ts.getText());
+        } else if (selection instanceof IMarkSelection) {
+            IMarkSelection ms = (IMarkSelection)selection;
+            try {
+                showText(ms.getDocument().get(ms.getOffset(), ms.getLength()));
+            } catch (BadLocationException ble) {
+            }
+        } else {
+            showText("Unsupported object type: " + selection.getClass().getName());
+        }
+    }
+
+    private String getClassNames(Object[] objects) {
+
+        StringBuilder classNames = new StringBuilder();
+        for (Object object : objects) {
+            classNames.append(object.getClass().getName());
+            classNames.append("\n");
+        }
+
+        return classNames.toString();
+    }
+
+    private void showItems(Object[] items) {
+        tableviewer.setInput(items);
+        pagebook.showPage(tableviewer.getControl());
+    }
+
+    private void showText(String text) {
+        textviewer.setDocument(new Document(text));
+        pagebook.showPage(textviewer.getControl());
+    }
+
+    public void createPartControl(Composite parent) {
+        // the PageBook allows simple switching between two viewers
+        pagebook = new PageBook(parent, SWT.NONE);
+
+        tableviewer = new TableViewer(pagebook, SWT.NONE);
+        tableviewer.setLabelProvider(new WorkbenchLabelProvider());
+        tableviewer.setContentProvider(new ArrayContentProvider());
+
+        // we're cooperative and also provide our selection
+        // at least for the tableviewer
+        getSite().setSelectionProvider(tableviewer);
+
+        textviewer = new TextViewer(pagebook, SWT.H_SCROLL | SWT.V_SCROLL);
+        textviewer.setEditable(false);
+
+        getSite().getWorkbenchWindow().getSelectionService().addPostSelectionListener(listener);
+    }
+
+    public void setFocus() {
+        pagebook.setFocus();
+    }
+
+    public void dispose() {
+        // important: We need do unregister our listener when the view is
+        // disposed
+        getSite().getWorkbenchWindow().getSelectionService().removePostSelectionListener(listener);
+        super.dispose();
+    }
+
+}
