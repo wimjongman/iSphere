5135ab49e780261bccc29685eb86228a8fa77d86
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2017-02-17 18:03:20 +0000

    Changed to catch "Throwable" instead of "Exception".

--- rse2 (RDi)/iSphere 5250 Emulator Base Plugin (RDi)/src/org/tn5250j/framework/tn5250/tnvt.java
+++ rse2 (RDi)/iSphere 5250 Emulator Base Plugin (RDi)/src/org/tn5250j/framework/tn5250/tnvt.java
@@ -348,7 +348,7 @@ public final class tnvt implements Runnable {
             me = new Thread(this);
             me.start();
 
-        } catch (Exception exception) {
+        } catch (Throwable exception) {
 
             if (exception.getMessage() == null) {
                 exception.printStackTrace();
