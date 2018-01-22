b199b27f7f7e2c82be8c7d1249c71e30e826c6e3
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2018-01-22 13:59:21 +0000

    Added description to JournalEntryType.

--- eclipse/iSphere Journal Explorer Core Plugin/src/biz/isphere/journalexplorer/core/model/JournalEntryType.java
+++ eclipse/iSphere Journal Explorer Core Plugin/src/biz/isphere/journalexplorer/core/model/JournalEntryType.java
@@ -12,17 +12,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum JournalEntryType {
-    BR, // Before rollback
-    DL, // Delete
-    DR, // Delete rollback
-    IL, // Increment record limit
-    PT, // Put
-    PX, // Put (reuse deleted record)
-    UB, // Update before
-    UP, // Update past
-    UR; // Update rollback
+    BR ("Before-image of record updated for rollback"),
+    DL ("Record deleted from physical file member"),
+    DR ("Record deleted for rollback"),
+    IL ("Increment record limit"),
+    PT ("Record added to physical file member"),
+    PX ("Record added directly to physical file member"),
+    UB ("Before-image of record updated in physical file member"),
+    UP ("After-image of record updated in physical file member"),
+    UR ("After-image of record updated for rollback");
 
     private static Map<String, JournalEntryType> values;
+
+    private String description;
 
     static {
         values = new HashMap<String, JournalEntryType>();
@@ -33,5 +35,18 @@ public enum JournalEntryType {
 
     public static JournalEntryType find(String value) {
         return values.get(value);
+    }
+
+    private JournalEntryType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, (%s)", getDescription(), this.name());
     }
 }
