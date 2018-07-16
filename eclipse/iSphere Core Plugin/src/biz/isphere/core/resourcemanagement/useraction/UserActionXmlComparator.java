a3d9021289e61f63cd058ec595122989576e8558
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2018-07-16 13:38:14 +0000

    Changed ordering of filters, user actions and compile commands when saving these objects to XML.

---
+++ eclipse/iSphere Core Plugin/src/biz/isphere/core/resourcemanagement/useraction/UserActionXmlComparator.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2018 iSphere Project Team
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *******************************************************************************/
+
+package biz.isphere.core.resourcemanagement.useraction;
+
+import java.util.Comparator;
+
+public class UserActionXmlComparator implements Comparator<RSEUserAction> {
+
+    public int compare(RSEUserAction o1, RSEUserAction o2) {
+
+        if (o1 == null && o2 == null) {
+            return 0;
+        } else if (o2 == null) {
+            return 1;
+        } else if (o1 == null) {
+            return -1;
+        }
+
+        if (o1.getOrder() > o2.getOrder()) {
+            return 1;
+        } else if (o1.getOrder() < o2.getOrder()) {
+            return -1;
+        } else {
+            o1.getLabel().compareTo(o2.getLabel());
+        }
+
+        return 0;
+    }
+
+}
