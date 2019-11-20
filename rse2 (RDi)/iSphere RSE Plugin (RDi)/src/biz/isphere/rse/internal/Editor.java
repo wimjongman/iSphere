bcd88675f029ff94555a1b505796cb69d3a848e9
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2019-11-20 19:00:56 +0000

    Fixed java.lang.ClassCastException when opening a member from the iSphere search result view for the second time.

--- rse2 (RDi)/iSphere RSE Plugin (RDi)/src/biz/isphere/rse/internal/Editor.java
+++ rse2 (RDi)/iSphere RSE Plugin (RDi)/src/biz/isphere/rse/internal/Editor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2014 iSphere Project Owners
+ * Copyright (c) 2012-2019 iSphere Project Owners
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -134,9 +134,11 @@ public class Editor implements IEditor {
                     IEditorPart part = editor.getEditor(false);
                     if (part != null) {
                         IEditorInput input = part.getEditorInput();
-                        IFileEditorInput fileInput = (IFileEditorInput)input;
-                        if (localFileResource.equals(fileInput.getFile())) {
-                            return part;
+                        if (input instanceof IFileEditorInput) {
+                            IFileEditorInput fileInput = (IFileEditorInput)input;
+                            if (localFileResource.equals(fileInput.getFile())) {
+                                return part;
+                            }
                         }
                     }
                 }
