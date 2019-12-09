01fc44f574216f3df27662c85247c374f491d282
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2019-12-09 18:31:10 +0000

    Removed obsolete object attribute "tree" from JournalEntryMenuAdapter.

--- eclipse/iSphere Journal Explorer Core Plugin/src/biz/isphere/journalexplorer/core/ui/popupmenus/JournalEntryMenuAdapter.java
+++ eclipse/iSphere Journal Explorer Core Plugin/src/biz/isphere/journalexplorer/core/ui/popupmenus/JournalEntryMenuAdapter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2018 iSphere Project Owners
+ * Copyright (c) 2012-2019 iSphere Project Owners
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,9 +7,6 @@
  *******************************************************************************/
 
 package biz.isphere.journalexplorer.core.ui.popupmenus;
-
-import java.util.LinkedList;
-import java.util.List;
 
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.StructuredSelection;
@@ -22,17 +19,13 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Tree;
-import org.eclipse.swt.widgets.TreeItem;
 
-import biz.isphere.journalexplorer.core.model.adapters.JournalProperties;
 import biz.isphere.journalexplorer.core.ui.actions.CompareSideBySideAction;
 import biz.isphere.journalexplorer.core.ui.actions.ExportToExcelAction;
 import biz.isphere.journalexplorer.core.ui.labelproviders.JournalEntryLabelProvider;
 
 public class JournalEntryMenuAdapter extends MenuAdapter {
 
-    private Tree tree;
     private TableViewer tableViewer;
     private Menu menuTableMembers;
     private Shell shell;
@@ -40,7 +33,6 @@ public class JournalEntryMenuAdapter extends MenuAdapter {
     private MenuItem exportToExcelMenuItem;
 
     public JournalEntryMenuAdapter(Menu menuTableMembers, TableViewer tableViewer) {
-        this.tree = null;
         this.tableViewer = tableViewer;
         this.shell = tableViewer.getControl().getShell();
         this.menuTableMembers = menuTableMembers;
@@ -62,17 +54,6 @@ public class JournalEntryMenuAdapter extends MenuAdapter {
     }
 
     private StructuredSelection getSelection() {
-
-        if (tree != null) {
-            List<JournalProperties> journalProperties = new LinkedList<JournalProperties>();
-            for (TreeItem treeItem : tree.getSelection()) {
-                Object data = treeItem.getData();
-                if (data instanceof JournalProperties) {
-                    journalProperties.add((JournalProperties)treeItem.getData());
-                }
-            }
-            return new StructuredSelection(journalProperties.toArray(new JournalProperties[journalProperties.size()]));
-        }
 
         ISelection selection = tableViewer.getSelection();
         if (selection instanceof StructuredSelection) {
