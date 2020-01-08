786f3c148d6a0d42759d1ddaa776f242361d90a0
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2020-01-08 19:42:19 +0000

    Removed debug console output.

--- eclipse/iSphere Core Plugin/src/biz/isphere/core/resourcemanagement/useraction/UserActionShowDifferencesDialog.java
+++ eclipse/iSphere Core Plugin/src/biz/isphere/core/resourcemanagement/useraction/UserActionShowDifferencesDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2018 iSphere Project Team
+ * Copyright (c) 2012-2020 iSphere Project Team
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -236,7 +236,6 @@ public class UserActionShowDifferencesDialog extends XDialog {
 
             CompareItem compareItem = (CompareItem)object;
             if (setBackgroundColor && !compareItem.isEqual()) {
-                System.out.println("getBackground: " + compareItem.getProperty());
                 return Display.getCurrent().getSystemColor(SWT.COLOR_YELLOW);
             }
 
