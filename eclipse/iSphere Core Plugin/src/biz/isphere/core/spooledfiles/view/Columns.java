4197c06ce415ca283957415e5f294eade112d9a6
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2020-01-17 16:10:47 +0000

    Changed ordering of columns in "Spooled Files" view and added column heading to "Columns" enumeration.

--- eclipse/iSphere Core Plugin/src/biz/isphere/core/spooledfiles/view/Columns.java
+++ eclipse/iSphere Core Plugin/src/biz/isphere/core/spooledfiles/view/Columns.java
@@ -11,34 +11,38 @@ package biz.isphere.core.spooledfiles.view;
 import java.util.ArrayList;
 import java.util.List;
 
+import biz.isphere.core.Messages;
+
 public enum Columns {
 
-    STATUS ("status", Index.STATUS, 70), //$NON-NLS-1$
-    FILE ("file", Index.FILE, 100), //$NON-NLS-1$
-    FILE_NUMBER ("fileNumber", Index.FILE_NUMBER, 80), //$NON-NLS-1$
-    JOB_NAME ("jobName", Index.JOB_NAME, 100), //$NON-NLS-1$
-    JOB_USER ("jobUser", Index.JOB_USER, 100), //$NON-NLS-1$
-    JOB_NUMBER ("jobNumber", Index.JOB_NUMBER, 100), //$NON-NLS-1$
-    JOB_SYSTEM ("jobSystem", Index.JOB_SYSTEM, 80), //$NON-NLS-1$
-    CREATION_DATE ("creationDate", Index.CREATION_DATE, 80), //$NON-NLS-1$
-    CREATION_TIME ("creationTime", Index.CREATION_TIME, 80), //$NON-NLS-1$
-    OUTPUT_QUEUE ("outputQueue", Index.OUTPUT_QUEUE, 160), //$NON-NLS-1$
-    OUTPUT_PRIORITY ("outputPriority", Index.OUTPUT_PRIORITY, 80), //$NON-NLS-1$
-    USER_DATE ("userData", Index.USER_DATE, 80), //$NON-NLS-1$
-    FORM_TYPE ("formType", Index.FORM_TYPE, 80), //$NON-NLS-1$
-    COPIES ("copies", Index.COPIES, 50), //$NON-NLS-1$
-    PAGES ("pages", Index.PAGES, 50), //$NON-NLS-1$
-    CURRENT_PAGE ("currentPage", Index.CURRENT_PAGE, 50), //$NON-NLS-1$
-    CREATION_TIMESTAMP ("creationTimestamp", Index.CREATION_TIMESTAMP, 150); //$NON-NLS-1$
+    STATUS ("status", ColumnIndex.STATUS, 70, Messages.Status), //$NON-NLS-1$
+    FILE ("file", ColumnIndex.FILE, 100, Messages.File), //$NON-NLS-1$
+    FILE_NUMBER ("fileNumber", ColumnIndex.FILE_NUMBER, 80, Messages.File_number), //$NON-NLS-1$
+    JOB_NAME ("jobName", ColumnIndex.JOB_NAME, 100, Messages.Job_name), //$NON-NLS-1$
+    JOB_USER ("jobUser", ColumnIndex.JOB_USER, 100, Messages.Job_user), //$NON-NLS-1$
+    JOB_NUMBER ("jobNumber", ColumnIndex.JOB_NUMBER, 100, Messages.Job_number), //$NON-NLS-1$
+    JOB_SYSTEM ("jobSystem", ColumnIndex.JOB_SYSTEM, 80, Messages.Job_system), //$NON-NLS-1$
+    CREATION_TIMESTAMP ("creationTimestamp", ColumnIndex.CREATION_TIMESTAMP, 150, Messages.Creation_timestamp), //$NON-NLS-1$
+    OUTPUT_QUEUE ("outputQueue", ColumnIndex.OUTPUT_QUEUE, 160, Messages.Output_queue), //$NON-NLS-1$
+    OUTPUT_PRIORITY ("outputPriority", ColumnIndex.OUTPUT_PRIORITY, 80, Messages.Output_priority), //$NON-NLS-1$
+    USER_DATA ("userData", ColumnIndex.USER_DATE, 80, Messages.User_data), //$NON-NLS-1$
+    FORM_TYPE ("formType", ColumnIndex.FORM_TYPE, 80, Messages.Form_type), //$NON-NLS-1$
+    COPIES ("copies", ColumnIndex.COPIES, 50, Messages.Copies), //$NON-NLS-1$
+    PAGES ("pages", ColumnIndex.PAGES, 50, Messages.Pages), //$NON-NLS-1$
+    CURRENT_PAGE ("currentPage", ColumnIndex.CURRENT_PAGE, 50, Messages.Current_page), //$NON-NLS-1$
+    CREATION_DATE ("creationDate", ColumnIndex.CREATION_DATE, 80, Messages.Creation_date), //$NON-NLS-1$
+    CREATION_TIME ("creationTime", ColumnIndex.CREATION_TIME, 80, Messages.Creation_time); //$NON-NLS-1$
 
     public final String name;
     public final int index;
     public final int width;
+    public final String label;
 
-    private Columns(String name, int columnNumber, int width) {
+    private Columns(String name, int columnIndex, int width, String label) {
         this.name = name;
-        this.index = columnNumber;
+        this.index = columnIndex;
         this.width = width;
+        this.label = label;
     }
 
     public static String[] names() {
@@ -51,7 +55,10 @@ public enum Columns {
         return names.toArray(new String[names.size()]);
     }
 
-    public interface Index {
+    /**
+     * Specifies the order of the table columns from left to right.
+     */
+    public interface ColumnIndex {
         public static int STATUS = 0;
         public static int FILE = 1;
         public static int FILE_NUMBER = 2;
@@ -59,16 +66,16 @@ public enum Columns {
         public static int JOB_USER = 4;
         public static int JOB_NUMBER = 5;
         public static int JOB_SYSTEM = 6;
-        public static int CREATION_DATE = 7;
-        public static int CREATION_TIME = 8;
-        public static int OUTPUT_QUEUE = 9;
-        public static int OUTPUT_PRIORITY = 10;
-        public static int USER_DATE = 11;
-        public static int FORM_TYPE = 12;
-        public static int COPIES = 13;
-        public static int PAGES = 14;
-        public static int CURRENT_PAGE = 15;
-        public static int CREATION_TIMESTAMP = 16;
+        public static int CREATION_TIMESTAMP = 7;
+        public static int OUTPUT_QUEUE = 8;
+        public static int OUTPUT_PRIORITY = 9;
+        public static int USER_DATE = 10;
+        public static int FORM_TYPE = 11;
+        public static int COPIES = 12;
+        public static int PAGES = 13;
+        public static int CURRENT_PAGE = 14;
+        public static int CREATION_DATE = 15;
+        public static int CREATION_TIME = 16;
     }
 
 }
