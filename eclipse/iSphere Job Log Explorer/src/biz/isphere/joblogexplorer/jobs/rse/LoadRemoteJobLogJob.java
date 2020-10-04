201d44170a6cd83379277fa01c08bf452df20716
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2020-10-04 12:36:52 +0000

    Added constructor to pass a qualified job name.

--- eclipse/iSphere Job Log Explorer/src/biz/isphere/joblogexplorer/jobs/rse/LoadRemoteJobLogJob.java
+++ eclipse/iSphere Job Log Explorer/src/biz/isphere/joblogexplorer/jobs/rse/LoadRemoteJobLogJob.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2019 iSphere Project Owners
+ * Copyright (c) 2012-2020 iSphere Project Owners
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,8 @@
  *******************************************************************************/
 
 package biz.isphere.joblogexplorer.jobs.rse;
+
+import java.util.StringTokenizer;
 
 import biz.isphere.core.ISpherePlugin;
 import biz.isphere.joblogexplorer.editor.JobLogExplorerJobInput;
@@ -17,6 +19,22 @@ public class LoadRemoteJobLogJob extends AbstractLoadInputJob {
     private String jobName;
     private String userName;
     private String jobNumber;
+
+    public LoadRemoteJobLogJob(String connectionName, String qualifiedJobName) {
+
+        this.connectionName = connectionName;
+
+        StringTokenizer tokens = new StringTokenizer(qualifiedJobName, " /");
+        if (tokens.hasMoreTokens()) {
+            this.jobNumber = tokens.nextToken();
+        }
+        if (tokens.hasMoreTokens()) {
+            this.userName = tokens.nextToken();
+        }
+        if (tokens.hasMoreTokens()) {
+            this.jobName = tokens.nextToken();
+        }
+    }
 
     public LoadRemoteJobLogJob(String connectionName, String jobName, String userName, String jobNumber) {
         this.connectionName = connectionName;
