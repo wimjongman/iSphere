736ba2eb9d19373ff8ef2fa25ae04a5e00654e4c
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2020-10-06 16:20:29 +0000

    Added menu option to the "Job" node of the debug view to copy the qualified job name to the clipboard, when a program stops at a breakpoint.

---
+++ rse2 (RDi)/iSphere RSE Plugin (RDi)/src/biz/isphere/rse/actions/CopyQualifiedJobNameDebugPopupAction.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2020 iSphere Project Team
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *******************************************************************************/
+
+package biz.isphere.rse.actions;
+
+import org.eclipse.debug.core.model.IProcess;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+
+import biz.isphere.base.internal.ClipboardHelper;
+import biz.isphere.core.ISpherePlugin;
+import biz.isphere.core.Messages;
+import biz.isphere.core.internal.QualifiedJobName;
+import biz.isphere.core.internal.viewmanager.IPinableView;
+import biz.isphere.core.internal.viewmanager.IViewManager;
+import biz.isphere.rse.ISphereRSEPlugin;
+import biz.isphere.rse.spooledfiles.view.WorkWithSpooledFilesView;
+import biz.isphere.rse.spooledfiles.view.rse.WorkWithSpooledFilesJobInputData;
+
+import com.ibm.debug.pdt.internal.core.model.DebuggeeProcess;
+
+@SuppressWarnings("restriction")
+public class CopyQualifiedJobNameDebugPopupAction implements IViewActionDelegate {
+
+    private Shell shell;
+    private IStructuredSelection structuredSelection;
+
+    public void run(IAction action) {
+
+        Object selectedObject = structuredSelection.getFirstElement();
+
+        if (selectedObject instanceof DebuggeeProcess) {
+
+            DebuggeeProcess debuggeeProcess = (DebuggeeProcess)selectedObject;
+            QualifiedJobName qualifiedJobName = QualifiedJobName.parse(debuggeeProcess.getAttribute(null));
+
+            if (isValid(qualifiedJobName)) {
+                ClipboardHelper.setText(qualifiedJobName.getQualifiedJobName());
+            }
+        }
+    }
+
+    protected void openWorkWithSpooledFilesView(String connectionName, QualifiedJobName qualifiedJobName, IWorkbenchPage page) {
+
+        try {
+
+            WorkWithSpooledFilesJobInputData inputData = new WorkWithSpooledFilesJobInputData(connectionName, qualifiedJobName);
+
+            String contentId = inputData.getContentId();
+            IViewManager viewManager = ISphereRSEPlugin.getDefault().getViewManager(IViewManager.SPOOLED_FILES_VIEWS);
+            IPinableView view = (IPinableView)viewManager.getView(WorkWithSpooledFilesView.ID, contentId);
+
+            if (view instanceof WorkWithSpooledFilesView) {
+                WorkWithSpooledFilesView wrkSplfView = (WorkWithSpooledFilesView)view;
+                wrkSplfView.setInputData(inputData);
+            }
+
+        } catch (Exception e) {
+            ISpherePlugin.logError(e.getMessage(), e);
+            MessageDialog.openError(getShell(), Messages.E_R_R_O_R, e.getLocalizedMessage());
+        }
+    }
+
+    public void selectionChanged(IAction action, ISelection selection) {
+
+        if (isIBMiJob(selection)) {
+            structuredSelection = ((IStructuredSelection)selection);
+            action.setEnabled(true);
+        } else {
+            structuredSelection = null;
+            action.setEnabled(false);
+        }
+    }
+
+    private boolean isValid(QualifiedJobName qualifiedJobName) {
+
+        if (qualifiedJobName == null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isIBMiJob(ISelection selection) {
+
+        if (selection instanceof IStructuredSelection) {
+            structuredSelection = ((IStructuredSelection)selection);
+            Object selectedObject = structuredSelection.getFirstElement();
+            if ((selectedObject instanceof IProcess)) {
+                IProcess process = (IProcess)selectedObject;
+                if (QualifiedJobName.parse(process.getAttribute(null)) != null) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void init(IViewPart view) {
+        this.shell = view.getSite().getShell();
+    }
+
+    private Shell getShell() {
+        return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+    }
+}
