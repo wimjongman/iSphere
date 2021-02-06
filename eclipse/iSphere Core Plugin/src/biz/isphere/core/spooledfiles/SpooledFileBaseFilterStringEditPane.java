817a111d951a7482f9659578c9365f64b58f3884
tools400 <tools400@b80707e1-d4e2-4098-9cf0-108009fc4daf>
2021-02-06 16:49:51 +0000

    Removed leading spaces from field name in error message.

--- eclipse/iSphere Core Plugin/src/biz/isphere/core/spooledfiles/SpooledFileBaseFilterStringEditPane.java
+++ eclipse/iSphere Core Plugin/src/biz/isphere/core/spooledfiles/SpooledFileBaseFilterStringEditPane.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2020 iSphere Project Owners
+ * Copyright (c) 2012-2021 iSphere Project Owners
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -519,14 +519,14 @@ public class SpooledFileBaseFilterStringEditPane {
             return error;
         }
 
-        error = validateSimple(Messages.___Library, outqLibText.getText(), SPECIAL_VALUES_OUTPUT_QUEUE_LIBRARY);
+        error = validateSimple(Messages.Library, outqLibText.getText(), SPECIAL_VALUES_OUTPUT_QUEUE_LIBRARY);
         if (error != null) {
             return error;
         }
 
         if ("*".equals(outqText.getText()) || "*ALL".equals(outqText.getText())) {
             if (!"*".equals(outqLibText.getText())) {
-                return Messages.bind(Messages.The_value_in_field_A_is_not_valid, Messages.___Library);
+                return Messages.bind(Messages.The_value_in_field_A_is_not_valid, Messages.Library);
             }
         }
 
